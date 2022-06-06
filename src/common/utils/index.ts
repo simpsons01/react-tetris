7bd8e5a058dd96338f3d30a78895025b7e6a8b44
simpsons01 <ilikekobe01@gmail.com>
2022-06-06 15:13:43 +0800

    add animation utils

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -7,3 +7,40 @@ export const setRef = <T = any>(ref: React.MutableRefObject<T>, val: T) => {
 export const getKeys = <T extends object, K extends keyof T>(obj: T): Array<K> => {
   return Object.keys(obj) as Array<K>;
 };
+
+export const createAnimation = (fn: (elapse: number) => void, onEnd?: Function, duration: number = 1) => {
+  let initialTimestamp: number | undefined,
+    elapse: number | undefined,
+    id: number | undefined,
+    _duration: number = duration;
+  return {
+    start: function startAnimation(timestamp?: number) {
+      initialTimestamp = initialTimestamp === undefined ? performance.now() : initialTimestamp;
+      timestamp = timestamp === undefined ? performance.now() : timestamp;
+      elapse = (timestamp - initialTimestamp) / 1000;
+      fn(elapse);
+      if (elapse < _duration) {
+        id = window.requestAnimationFrame(startAnimation);
+      } else {
+        if (typeof onEnd == "function") {
+          onEnd();
+        }
+      }
+    },
+    pause: function pauseAnimation() {
+      if (id === undefined) return;
+      _duration = duration - (elapse as number);
+      initialTimestamp = undefined;
+      elapse = undefined;
+      window.cancelAnimationFrame(id);
+      id = undefined;
+    },
+    reset: function resetAnimation() {
+      _duration = duration;
+      initialTimestamp = undefined;
+      elapse = undefined;
+      if (id !== undefined) window.cancelAnimationFrame(id);
+      id = undefined;
+    },
+  };
+};
