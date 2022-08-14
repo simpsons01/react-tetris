f80a8e39e8a33414b33274ee541c056794f90d80
simpsons01 <ilikekobe01@gmail.com>
2022-08-14 17:21:42 +0800

    新增共用Modal元件

---
+++ src/components/Modal/base.tsx
@@ -0,0 +1,97 @@
+import React from "react";
+import { createPortal } from "react-dom";
+import styled from "styled-components";
+
+const Overlay = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  z-index: 1000;
+  left: 0;
+  top: 0;
+  background-color: rgba(0, 0, 0, 0.6);
+`;
+
+const Container = styled.div`
+  position: absolute;
+  min-width: 400px;
+  padding: 32px;
+  background-color: #fff;
+  z-index: 1;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1;
+`;
+
+const Header = styled.div`
+  font-size: 24px;
+  line-height: 24px;
+`;
+
+const Body = styled.div`
+  margin-top: 16px;
+`;
+
+const Footer = styled.div`
+  margin-top: 16px;
+  text-align: center;
+
+  .cancel {
+    margin-right: 24px;
+  }
+`;
+
+export interface IModal {
+  isOpen: boolean;
+  title?: string;
+  body: React.ReactNode;
+  confirm?: {
+    text: string;
+    onClick: (e: React.MouseEvent) => void;
+  };
+  cancel?: {
+    text: string;
+    onClick: (e: React.MouseEvent) => void;
+  };
+  mountEl?: HTMLElement;
+}
+
+const Modal = (props: IModal): JSX.Element | null => {
+  const {
+    isOpen,
+    title,
+    body,
+    confirm,
+    cancel,
+    mountEl = document.body,
+  } = props;
+
+  if (!isOpen) return null;
+
+  return createPortal(
+    <Overlay>
+      <Container className="nes-dialog is-rounded">
+        {title ? <Header>{title}</Header> : null}
+        <Body>{body}</Body>
+        {confirm || cancel ? (
+          <Footer>
+            {cancel ? (
+              <button className="cancel nes-btn" onClick={cancel.onClick}>
+                {cancel.text}
+              </button>
+            ) : null}
+            {confirm ? (
+              <button className="confirm nes-btn" onClick={confirm.onClick}>
+                {confirm.text}
+              </button>
+            ) : null}
+          </Footer>
+        ) : null}
+      </Container>
+    </Overlay>,
+    mountEl
+  );
+};
+
+export default Modal;
