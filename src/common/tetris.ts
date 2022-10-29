a4ff1e6a59bf4886c1e8b3d332cd918a2d3d9b15
simpsons01 <ilikekobe01@gmail.com>
2022-10-29 19:41:38 +0800

    調整score計算方式

--- src/common/tetris.ts
+++ src/common/tetris.ts
@@ -15,42 +15,25 @@ export const DISPLAY_ZONE_ROW_END = 39;
 export const getLevelByLine = (line: number): number => Math.floor(line / 10);
 
 export const getTetriminoFallingDelayByLevel = (level: number) => {
-  const frame = (() => {
-    const _: { [key: string]: number } = {
-      "0": 48,
-      "1": 43,
-      "2": 38,
-      "3": 33,
-      "4": 28,
-      "5": 23,
-      "6": 18,
-      "7": 13,
-      "8": 8,
-      "9": 6,
-      "10": 5,
-      "11": 5,
-      "12": 5,
-      "13": 4,
-      "14": 4,
-      "15": 4,
-      "16": 3,
-      "17": 3,
-      "18": 3,
-      "19": 2,
-      "20": 2,
-      "21": 2,
-      "22": 2,
-      "23": 2,
-      "24": 2,
-      "25": 2,
-      "26": 2,
-      "27": 2,
-      "28": 2,
-      "29": 1,
-    };
-    return _[`${level}`] ?? 1;
-  })();
-  return Math.floor(frame * (1 / 60) * 1000);
+  const sec = {
+    "0": 1000,
+    "1": 1000,
+    "2": 793,
+    "3": 618,
+    "4": 473,
+    "5": 355,
+    "6": 262,
+    "7": 190,
+    "8": 135,
+    "9": 94,
+    "10": 64,
+    "11": 43,
+    "12": 28,
+    "13": 18,
+    "14": 11,
+    "15": 7,
+  }[level];
+  return sec ?? 7;
 };
 
 export const getScoreByLevelAndLine = (level: number, line: number): number => {
